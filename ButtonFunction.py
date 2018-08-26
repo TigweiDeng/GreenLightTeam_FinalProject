6041b761b9b68a29a63089aab05923b9fd4cfa53
TigweiDeng <1250206983@qq.com>
2018-08-26 15:34:39 +0800

    交作业

--- ButtonFunction.py
+++ ButtonFunction.py
@@ -1,5 +1,6 @@
 
 from RootPage import *
+from FaceDetect import *
 
 def createHomePage(faceModelingBg, faceRecognizeBg):
     pageFrame()
@@ -11,13 +12,13 @@ def createHomePage(faceModelingBg, faceRecognizeBg):
     faceRecognize = Label(homePage, image = faceRecognizeBg, width = 302, height = 396)
     faceRecognize.place(x = 400, y = 30)
 
-    faceModelingButton = Button(homePage, text = '人脸建模\n(未开放)', bg = 'grey', font = ('微软雅黑',20), activebackground = 'grey')
+    faceModelingButton = Button(homePage, text = '人脸建模\n(未开放)', bg = 'pink', font = ('微软雅黑',20), activebackground = 'red', command = lambda : createRecognizePage(True))
     faceModelingButton.place(x = 101, y = 430, width = 200, height = 100)
 
     faceRecognizeButton = Button(homePage,  text = '人脸识别', bg = 'skyblue', font = ('微软雅黑',20), activebackground = 'blue', command = createRecognizePage)
     faceRecognizeButton.place(x = 451, y = 430, width = 200, height = 100)
 
-def createRecognizePage():
+def createRecognizePage(isModeling = False):
     homePage.destroy()
     faceRecognizePage.pack()
 
@@ -27,7 +28,7 @@ def createRecognizePage():
     choosePath = Button(faceRecognizePage, text = "路径选择", font = ('微软雅黑',10), command = lambda : selectPath(Format))
     choosePath.grid(row=0, column=1, stick=E, padx = 10) 
 
-    confirmButton = Button(faceRecognizePage, text = '确定', font = ('微软雅黑',10), command = confirm)
+    confirmButton = Button(faceRecognizePage, text = '确定', font = ('微软雅黑',10), command = lambda : confirm(path, isModeling))
     confirmButton.grid(row=1, stick=W, pady=10, padx = 10)
 
     backButton = Button(faceRecognizePage, text = '返回', font = ('微软雅黑',10), command = back)
@@ -45,8 +46,11 @@ def selectPath(format):
     FilePath = askopenfilename(filetypes = format)
     filePath.set(FilePath)
 
-def confirm():
-    print(FilePath)
+def confirm(path, isModeling):
+    if isModeling:
+        pass
+    else:
+        start(path.get())
 
 def back():
     faceRecognizePage.destroy()
